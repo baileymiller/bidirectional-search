afd62dc082c53c858de767517b91bbd6314d1047
Bailey M Miller <bmmiller@GS20052.SP.CS.CMU.EDU>
2022-03-13 03:29:22 -0400

    working

--- puzzle.py
+++ puzzle.py
@@ -1,7 +1,11 @@
 from tracemalloc import start
+from turtle import backward
+from unicodedata import bidirectional
 import numpy as np
 from heapq import heappush, heappop
 import random 
+
+REVERSE_ACTION = { 'l': 'r', 'r': 'l', 't': 'b', 'b': 't'}
 
 #=========================== State and Action Helpers =============================#
 class State:
@@ -55,7 +59,7 @@ class State:
     return np.array_equal(self.data, other_state.data)
 
   def __gt__(self, other_state):
-    return hash(self) > hash(other_state)
+    return str(self) > str(other_state)
 
   def __str__(self):
     return str(self.data)
@@ -91,6 +95,9 @@ class PriorityQueue:
       element.state == state
       for priority, element in self.elements
     )
+  
+  def size(self):
+    return len(self.elements)
 
 #=========================== Graph Search Algorithms =============================#
 def unidirectional_graph_search(start, goal, heuristic, priority):
@@ -126,6 +133,82 @@ def unidirectional_graph_search(start, goal, heuristic, priority):
     
   return curr_node.path, len(visited)
 
+def add_sucessors_to_frontier(curr_node, visited, visited_from, frontier, priority, heuristic, goal):
+  next_states = curr_node.state.expand()
+  for action in next_states.keys():
+    action_cost = next_states[action][0]
+    next_state = next_states[action][1]
+
+    if next_state == curr_node.state:
+      continue
+
+    if next_state in visited:
+      continue
+
+    next_path = curr_node.path.copy()
+    next_path.append(action)
+    next_node = PQNode(next_state, next_path, curr_node.g + action_cost)
+    next_node_priority = priority(next_node.g, heuristic(next_state, goal))
+    frontier.push(next_node, next_node_priority)
+    visited_from[str(next_state)] = (curr_node.state, action)
+
+def expand_next_node(visited, visited_from, frontier, other_visited, priority, heuristic, goal):
+  curr_node = frontier.pop()
+
+  if curr_node.state in other_visited:
+    return curr_node
+  
+  if curr_node in visited:
+    return None
+
+  visited.add(curr_node.state)
+
+  add_sucessors_to_frontier(curr_node, visited, visited_from, frontier, priority, heuristic, goal)
+  return None
+
+def bidirectional_graph_search(start, goal, heuristic, priority, select_direction):
+  visitedF = set()
+  visitedB = set()
+
+  visitedF.add(start)
+  visitedB.add(goal)
+
+  frontierF = PriorityQueue()
+  frontierB = PriorityQueue()
+
+  wf = {}
+  wt = {}
+
+  add_sucessors_to_frontier(PQNode(start.get_copy(), [], 0), visitedF, wf, frontierF, priority, heuristic, goal)
+  add_sucessors_to_frontier(PQNode(goal.get_copy(), [], 0), visitedB, wt, frontierB, priority, heuristic, start)
+
+  while True:
+    direction = select_direction(visitedF, visitedB, frontierF, frontierB)
+    if direction == 'forward':
+      intersect_node = expand_next_node(visitedF, wf, frontierF, visitedB, priority, heuristic, goal)
+    else:
+      intersect_node = expand_next_node(visitedB, wt, frontierB, visitedF, priority, heuristic, start)
+
+    if not intersect_node == None:
+      forward_path = []
+      curr_state = intersect_node.state 
+      while not curr_state == start:
+        prev_state, action = wf[str(curr_state)]
+        forward_path.append(action)
+        curr_state = prev_state
+          
+      backward_path = []
+      curr_state = intersect_node.state
+      while not curr_state == goal:
+        next_state, action = wt[str(curr_state)]
+        backward_path.append(REVERSE_ACTION[action])
+        curr_state = next_state
+
+      path = list(reversed(forward_path)) + backward_path
+      break
+
+  return path, len(visitedF) + len(visitedB)
+
 #=========================== Heuristics =============================#
 def h_trivial(state, goal):
   return 0
@@ -134,10 +217,25 @@ def h_trivial(state, goal):
 def ucs_priority(g, h):
     return g
 
-#=========================== Solvers ================================#
-def ucs_forward(start, goal):
-  return unidirectional_graph_search(start, goal, h_trivial, ucs_priority)
+#=========Forward/Backward Expansion Selection Strategy=============#
+def only_forward(_visitedF, _visitedB, _frontierF, _frontierB):
+  return 'forward'
 
+def only_backward(_visitedF, _visitedB, _frontierF, _frontierB):
+  return 'backward'
+
+def random_direction(_visitedF, _visitedB, _frontierF, _frontierB):
+  return 'forward' if random.random() < 0.5 else 'backward'
+
+#=========================== Solvers ================================#
+def ucs_unidirectional_forward(start, goal):
+  return bidirectional_graph_search(start, goal, h_trivial, ucs_priority, only_forward)
+
+def ucs_unidirectional_backward(start, goal):
+  return bidirectional_graph_search(start, goal, h_trivial, ucs_priority, only_backward)
+
+def ucs_bidirectional(start, goal):
+  return bidirectional_graph_search(start, goal, h_trivial, ucs_priority, random_direction)
 
 #=========================== Test Helpers ================================#
 def apply_random_valid_action(state):
@@ -162,8 +260,41 @@ def validate_path(path, start_state, goal_state):
 
 def test_random():
   goal_state = get_random_goal_state()
-  start_state = get_random_state_from_goal(goal_state, 23)
+  start_state = get_random_state_from_goal(goal_state, 35)
   print(start_state, '\n', goal_state)
-  path, nodes_visited = ucs_forward(start_state, goal_state)
+  path, nodes_visited = ucs_bidirectional(start_state, goal_state)
   print('path: ', path, '   nodes_visited: ', nodes_visited)
   print('Path is valid: ', validate_path(path, start_state, goal_state))
+
+def test_fixed():
+  start_state = State(np.array([
+    [11,  4,  7,  5],
+    [ 1,  8, 13,  0],
+    [ 9,  3, 12, 14],
+    [ 2, 15,  6, 10]
+  ]))
+
+  goal_state = State(np.array([
+    [11,  4,  7,  5],
+    [ 1,  3,  8, 13],
+    [ 2,  9, 12, 14],
+    [15,  6,  0, 10]
+  ]))
+
+  f_path, f_nodes_visited = ucs_unidirectional_forward(start_state, goal_state)
+  b_path, b_nodes_visited = ucs_unidirectional_backward(start_state, goal_state)
+  bi_path, bi_nodes_visited = ucs_bidirectional(start_state, goal_state)
+
+  assert(validate_path(f_path, start_state, goal_state))
+  assert(validate_path(b_path, start_state, goal_state))
+  assert(validate_path(bi_path, start_state, goal_state))
+
+  assert(len(f_path) == len(bi_path) and len(f_path) == len(b_path))
+
+  print('Forward cost: ', f_nodes_visited)
+  print('Backward cost: ', b_nodes_visited)
+  print('Bi costs', bi_nodes_visited)
+
+
+
+test_fixed()
